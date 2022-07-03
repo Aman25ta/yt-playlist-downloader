30dfe0171b08ffba1f6f4092bd89cf2b571a102c
Aman25ta <aman25.ta@gmail.com>
2022-07-03 11:33:25 +0300

    yeah yeah

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
 import pytube
 import re,os
 
+import eyed3
+from eyed3.id3.frames import ImageFrame
+import subprocess
+import requests
+
+import time
+
+def process(song,artist,link,title,track_num):
+   time.sleep(5)
+
+   '''fmpeg -i "./songs/{title}".mp4 "./songs/{track_num}. {title}".mp3'''
+   ppath = song.replace('.mp4','')
+   ppath = ppath.replace('.mp3','')
+   subprocess.run(f'C:/ffmpeg/bin/ffmpeg.exe -i \"{ppath}\".mp4 \"{ppath}\".mp3',shell=True)
+
+   os.remove(f"{ppath}.mp4")
+
+   newname = song.replace('.mp4', '.mp3')
+   '''
+   oldbase = os.path.splitext(song)
+   output = os.rename(song, newname)
+   '''
+   
+   img_data = requests.get(link).content
+   with open('cover.jpg', 'wb') as handler:
+       handler.write(img_data)
+      
+   print(newname)
+   audiofile = eyed3.load(newname)
+   
+   if (audiofile.tag == None):
+       audiofile.initTag()
+   
+   audiofile.tag.title = title
+   audiofile.tag.images.set(ImageFrame.FRONT_COVER, open('cover.jpg','rb').read(), 'image/jpeg')
+   audiofile.tag.artist = artist
+   audiofile.tag.track_num = track_num
+   audiofile.tag.save()
+   
+   os.remove('cover.jpg')
 
 xurl = str(input("Enter url: "))
 
@@ -12,6 +52,7 @@ if rng!="0":
    n2 = rng.split("-")[1].strip(" ")
 else:
    rng=0
+   n1=n2=0
 try:
    n1=int(n1)
    n2=int(n2)
@@ -21,23 +62,18 @@ except:
 for url in playlist:
    if rng==0:
       ytt = pytube.YouTube(url)
-      ytt.streams.get_audio_only().download("./songs",filename_prefix=f"{c}. ")
+      path = ytt.streams.get_audio_only().download("./songs",filename_prefix=f"{c}. ")
+      process("./songs/"+path.split('\\')[-1],ytt.author,ytt.thumbnail_url,ytt._title,c)
       print(f"Downloaded: {c}. "+ytt._title)
    else:
       if c>=n1 and c<=n2:
          ytt = pytube.YouTube(url)
-         ytt.streams.get_audio_only().download("./songs",filename_prefix=f"{c}. ")
+         path = ytt.streams.get_audio_only().download("./songs",filename_prefix=f"{c}. ")
+         process("./songs/"+path.split('\\')[-1],ytt.author,ytt.thumbnail_url,ytt._title,c)
          print(f"Downloaded: {c}. "+ytt._title)
       elif c>n2:
          break
    c+=1
 
-folder = './songs'
-print("Converting...")
-for filename in os.listdir(folder):
-    infilename = os.path.join(folder,filename)
-    if not os.path.isfile(infilename): continue
-    oldbase = os.path.splitext(filename)
-    newname = infilename.replace('.mp4', '.mp3')
-    output = os.rename(infilename, newname)
+folder = './songs' 
 print("Downloaded.")
