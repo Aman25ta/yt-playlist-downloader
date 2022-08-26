d5b3770708a52cb5ed2e9ab18629e5b131545fa8
aman25ta <aman25.ta@gmail.com>
2022-08-26 19:15:30 +0300

    Added mp4 support along with mp3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pytube
-import re,os
+import re,os,sys
 
 import eyed3
 from eyed3.id3.frames import ImageFrame
@@ -7,11 +7,16 @@ import subprocess
 import requests
 
 import time
+import random,string
 
-def process(song,artist,link,title,track_num):
+letters = string.ascii_lowercase
+uid = ''.join(random.choice(letters) for i in range(6))
+
+
+def process(song,artist,link,title,track_num,playlist_t):
    time.sleep(5)
 
-   '''fmpeg -i "./songs/{title}".mp4 "./songs/{track_num}. {title}".mp3'''
+   '''fmpeg -i "./songs/{playlist_t}/{title}".mp4 "./songs/{playlist_t}/{track_num}. {title}".mp3'''
    ppath = song.replace('.mp4','')
    ppath = ppath.replace('.mp3','')
    subprocess.run(f'C:/ffmpeg/bin/ffmpeg.exe -i \"{ppath}\".mp4 \"{ppath}\".mp3',shell=True)
@@ -25,7 +30,7 @@ def process(song,artist,link,title,track_num):
    '''
    
    img_data = requests.get(link).content
-   with open('cover.jpg', 'wb') as handler:
+   with open(f'cover_{uid}.jpg', 'wb') as handler:
        handler.write(img_data)
       
    print(newname)
@@ -35,14 +40,19 @@ def process(song,artist,link,title,track_num):
        audiofile.initTag()
    
    audiofile.tag.title = title
-   audiofile.tag.images.set(ImageFrame.FRONT_COVER, open('cover.jpg','rb').read(), 'image/jpeg')
+   audiofile.tag.images.set(ImageFrame.FRONT_COVER, open(f'cover_{uid}.jpg','rb').read(), 'image/jpeg')
    audiofile.tag.artist = artist
    audiofile.tag.track_num = track_num
    audiofile.tag.save()
    
-   os.remove('cover.jpg')
+   os.remove(f'cover_{uid}.jpg')
 
 xurl = str(input("Enter url: "))
+
+typ_dld = int(input("Enter 1 for mp3, 2 for mp4: "))
+if typ_dld != 1 and typ_dld != 2:
+   print("Invalid input")
+   sys.exit()
 
 playlist = pytube.Playlist(xurl)
 c=1
@@ -59,21 +69,41 @@ try:
 except:
    print("Invalid range.")
    rng = 0
-for url in playlist:
-   if rng==0:
-      ytt = pytube.YouTube(url)
-      path = ytt.streams.get_audio_only().download("./songs",filename_prefix=f"{c}. ")
-      process("./songs/"+path.split('\\')[-1],ytt.author,ytt.thumbnail_url,ytt._title,c)
-      print(f"Downloaded: {c}. "+ytt._title)
-   else:
-      if c>=n1 and c<=n2:
+if typ_dld == 1:
+   for url in playlist:
+      if rng==0:
          ytt = pytube.YouTube(url)
-         path = ytt.streams.get_audio_only().download("./songs",filename_prefix=f"{c}. ")
-         process("./songs/"+path.split('\\')[-1],ytt.author,ytt.thumbnail_url,ytt._title,c)
+         path = ytt.streams.get_audio_only().download(f"./songs/{playlist.title}",filename_prefix=f"{c}. ")
+         process(f"./songs/{playlist.title}/"+path.split('\\')[-1],ytt.author,ytt.thumbnail_url,ytt._title,c,playlist.title)
          print(f"Downloaded: {c}. "+ytt._title)
-      elif c>n2:
-         break
-   c+=1
-
-folder = './songs' 
+      else:
+         if c>=n1 and c<=n2:
+            ytt = pytube.YouTube(url)
+            path = ytt.streams.get_audio_only().download(f"./songs/{playlist.title}",filename_prefix=f"{c}. ")
+            process(f"./songs/{playlist.title}/"+path.split('\\')[-1],ytt.author,ytt.thumbnail_url,ytt._title,c,playlist.title)
+            print(f"Downloaded: {c}. "+ytt._title)
+         elif c>n2:
+            break
+      c+=1
+elif typ_dld ==2:
+   capts=input("Type 1 to download captions, if they exist: ")
+   for url in playlist:
+      if rng==0:
+         ytt = pytube.YouTube(url)
+         path = ytt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution().download(f'./videos/{playlist.title}',filename_prefix=f"{c}. ")
+         if capts =='1':
+            caption = yt.captions.get_by_language_code('en')
+            caption.download(srt=True,output_path=f'./videos/{playlist.title}')
+         print(f"Downloaded: {c}. "+ytt._title)
+      else:
+         if c>=n1 and c<=n2:
+            ytt = pytube.YouTube(url)
+            path = ytt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution().download(f'./videos/{playlist.title}',filename_prefix=f"{c}. ")
+            if capts =='1':
+               caption = yt.captions.get_by_language_code('en')
+               caption.download(srt=True,output_path=f'./videos/{playlist.title}')
+            print(f"Downloaded: {c}. "+ytt._title)
+         elif c>n2:
+            break
+      c+=1
 print("Downloaded.")
